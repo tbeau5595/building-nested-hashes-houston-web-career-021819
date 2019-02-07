c156a30f173a1e981132540230f513815a0a4945
Trey <tbeau5595@hotmail.com>
2019-02-07 21:50:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,38 +9,38 @@ def second_challenge
         name = "Lady Montague",
         age = "54"},
       :hero => {
-        :name = "Romeo",
-        :age = "15"
-        :status = "alive"},
+        name = "Romeo",
+        age = "15"
+        status = "alive"},
       :hero_friends [
-        {:name = "Benvolio",
-          :age = "17",
-          :attitude = "worried"},
-        {:name = "Mercutio",
-          :age = "18",
-          :attitude = "hot-headed"}        }
+        {name = "Benvolio",
+          age = "17",
+          attitude = "worried"},
+        {name = "Mercutio",
+          age = "18",
+          attitude = "hot-headed"}        }
         ]      
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {
-        :name = "Lord Capulet",
+        name = "Lord Capulet",
         age = "50",}
       :matriarch => {
-        :name = "Lady Capulet",
-        :age = "51"},
+        name = "Lady Capulet",
+        age = "51"},
       :heroine => {
-        :name = "JUliet",
-        :age = "15",
-        :status = "alive"
+        name = "JUliet",
+        age = "15",
+        status = "alive"
       },
       :heroine_friends => [
-        {:name = "Steven",
-          :age - "30",
-          :attitude = "confused"},
-        {:name = "Nurse",
-          :age = "44",
-          :attitude = "worried"}
+        {name = "Steven",
+          age - "30",
+          attitude = "confused"},
+        {name = "Nurse",
+         age = "44",
+          attitude = "worried"}
         ]      
       }}
 
